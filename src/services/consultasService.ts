eac4c50a652bc5b4908af2476bce9c669c20ee40
NickolasMorenoCardoso <nickolasmcardoso@gmail.com>
2026-08-13 22:24:23 -0300

    Validação de CPF, Máscaras e Formulário de Agendamento

--- src/services/consultasService.ts
+++ src/services/consultasService.ts
@@ -6,7 +6,7 @@
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Consulta } from "../interfaces/consulta";
-import { StatusConsulta } from "../types/statusConsulta";
+
 
 class ConsultasService {
   /**
